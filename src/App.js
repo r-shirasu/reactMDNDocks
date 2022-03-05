d787f5464ba6f99db1710b2498a115810f878699
r-shirasu <risako.shirasu@asia-quest.jp>
2022-03-06 01:09:54 +0900

    fix: remove ul role

--- src/App.js
+++ src/App.js
@@ -103,7 +103,6 @@ export const App = (props) => {
         {headingText}
       </h2>
       <ul
-        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
